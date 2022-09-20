59dd4ef71c5008b54d32adf8bafea30199b6853b
Niraina <stg-eni-niraina@livenexx.net>
2022-09-20 17:04:42 +0300

    100 Paginer les donnees grace 224 ApiPlatform

--- assets/app.js
+++ assets/app.js
@@ -16,6 +16,7 @@ import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import { HashRouter, Switch, Route } from "react-router-dom";
 import CustomerPage from './pages/CustomerPage';
+import CustomerPageWithPagination from './pages/CustomerPageWithPagination';
 
 const App = () => {
     return (
@@ -23,7 +24,7 @@ const App = () => {
         <Navbar />
         <main className="container pt-5">
             <Switch>
-                <Route path="/customer" component={CustomerPage}/>
+                <Route path="/customer" component={CustomerPageWithPagination}/>
                 <Route path="/" component={HomePage} />
             </Switch>
         </main>
---
+++ assets/pages/CustomerPageWithPagination.jsx
@@ -0,0 +1,101 @@
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import Pagination from '../components/Pagination';
+const CustomerPageWithPagination = (props) => {
+
+    const [customers, setCustomers] = useState([]);
+    const [totalItems, setTotalItems] = useState(0);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [loading, setLoading] = useState(true);
+    const itemsPerPage = 10;
+
+    useEffect(() => {
+        axios.get(`http://localhost:8000/api/customers?pagination=true&count=${itemsPerPage}&page=${currentPage}`)
+        .then(response => {
+            setCustomers(response.data['hydra:member']);
+            setTotalItems(response.data['hydra:totalItems']);
+            setLoading(false);
+        })
+        .catch(error => console.log(error.response));
+    }, [currentPage]);
+
+    const handleDelete = (id) => {
+        console.log(id);
+
+        const originalCustomers = [...customers];
+
+        //1. Approche optimiste
+        setCustomers(customers.filter(customer => customer.id !== id));
+        //2. Approche pessimiste
+
+        axios.delete("http://localhost:8000/api/customers/" + id)
+        .then(response => console.log("ok"))
+        .catch(error => {
+            setCustomers(originalCustomers);
+            console.log(error.response)
+        });
+    }
+
+    const handlePageChange = (page) => {
+        setCurrentPage(page);
+        setLoading(true);
+    }
+
+
+    const paginatedCustomers = Pagination.getData(customers, currentPage, itemsPerPage);
+
+    return ( 
+    <>
+        <h1>Liste des clients pagination</h1>
+        <table className="table table-hover">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Client</th>
+                    <th>Email</th>
+                    <th>Entreprise</th>
+                    <th className="text-center">Factures</th>
+                    <th className="text-center">Montant Total</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                {loading && 
+                (<tr>
+                    <td>
+                        Chargement ...
+                    </td>
+                </tr>)}
+                {!loading && customers.map(customer => 
+                <tr key={customer.id}>
+                    <td>{customer.id}</td>
+                    <td>
+                        <a href="#">{customer.firstName} {customer.lastName}</a>
+                    </td>
+                    <td>{customer.email}</td>
+                    <td>{customer.company}</td>
+                    <td className="text-center">
+                        <span className="badge bg-primary">
+                            {customer.invoices.length}
+                        </span>
+                    </td>
+                    <td className="text-center">{customer.totalAmount.toLocaleString()} €</td>
+                    <td>
+                        <button 
+                        onClick={() => handleDelete(customer.id)}
+                        disabled={ customer.invoices.length > 0 } 
+                        className="btn btn-sm btn-danger"
+                        >
+                            Supprimer
+                        </button>
+                    </td>
+                </tr>
+                )}
+            </tbody>
+        </table>
+        <Pagination currentPage={currentPage} itemsPerPage={itemsPerPage} length={totalItems} onPageChanged={handlePageChange}/>
+    </>
+    );
+}
+ 
+export default CustomerPageWithPagination;
